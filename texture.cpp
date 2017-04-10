4d5187264153d30e31e631c3124f48aca63252f8
IvanYossi <ghevan@gmail.com>
2017-04-09 19:57:00 -0500

    correct line endings WIN to UNIX

--- texture.cpp
+++ texture.cpp
@@ -9,13 +9,13 @@ Texture::Texture(const std::string& fileName)
 
     if(data == NULL)
 		std::cerr << "Unable to load texture: " << fileName << std::endl;
-        
+
     glGenTextures(1, &m_texture);
     glBindTexture(GL_TEXTURE_2D, m_texture);
-        
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        
+
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
